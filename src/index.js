8527d446a265c6867ff4b279e8fae749dddb1ed3
JeromeC7 <jerome.cornet@sncf.fr>
2023-06-30 13:34:08 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 // let message: string = 'Hello World !';
 // console.log(message);
 // console.log("test");
-
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // // number : Représente les nombres, qu'ils soient entiers ou à virgule flottante.
 // // string : Représente une séquence de caractères.
 // // boolean : Représente une valeur booléenne, soit true (vrai) soit false (faux).
@@ -10,7 +24,6 @@
 // // symbol : Représente un identifiant unique et immuable.
 // // bigint : Représente des entiers de grande taille.
 // // void : ne retourne rien
-
 // let tableau: number[];
 // let tableau2: string[];
 // let tableau3: boolean[];
@@ -21,7 +34,6 @@
 // let tableau8: void[];
 // let tableau9: Array<boolean>;
 // let tableau10: any[];
-
 // function maFonction(param1: number, param2: string): boolean {
 //   return true;
 // }
@@ -32,7 +44,6 @@
 // }
 // console.log('Test maFonction boolean:'+maFonction(1,"UN"));
 // console.log('Test maFonction2 void:'+maFonction2(1,"UN"));
-
 // // class MaClasse {
 // // }
 // // const instance = new MaClasse();
@@ -40,11 +51,9 @@
 // //   // L'instance est de type MaClasse
 // //   console.log('Ceci est une instance de MaClasse');
 // // }
-
 // // class MaClasse2 {
 // //   prop1: string;
 // //   prop2: number;
-
 // //   constructor(param1: string, param2: number) {
 // //     this.prop1 = param1;
 // //     this.prop2 = param2;
@@ -53,45 +62,37 @@
 // // const instance2 = new MaClasse2("valeur1", 42);
 // // console.log(instance2.prop1); // Affiche "valeur1"
 // // console.log(instance2.prop2); // Affiche 42
-
 // // class MaClasse {
 // //   public prop1: string;
 // //   private prop2: number;
 // //   protected prop3: boolean;
-
 // //   constructor(param1: string, param2: number, param3: boolean) {
 // //     this.prop1 = param1;
 // //     this.prop2 = param2;
 // //     this.prop3 = param3;
 // //   }
-
 // //   public methodePublique() {
 // //     console.log("Méthode publique");
 // //     console.log(this.prop1); // Accès à prop1 depuis l'intérieur de la classe
 // //     console.log(this.prop2); // Erreur : Prop2 est privée et ne peut pas être accédée ici
 // //     console.log(this.prop3); // Accès à prop3 depuis l'intérieur de la classe
 // //   }
-
 // //   private methodePrivee() {
 // //     console.log("Méthode privée");
 // //   }
-
 // //   protected methodeProtegee() {
 // //     console.log("Méthode protégée");
 // //   }
 // // }
-
 // // class MaClasseEnfant extends MaClasse {
 // //   constructor(param1: string, param2: number, param3: boolean) {
 // //     super(param1, param2, param3);
 // //   }
-
 // //   public utiliserProprietesProtegees() {
 // //     console.log(this.prop3); // Accès à prop3 depuis une classe enfant
 // //     this.methodeProtegee(); // Appel de la méthode protégée depuis une classe enfant
 // //   }
 // // }
-
 // // const instance = new MaClasse("valeur1", 42, true);
 // // console.log(instance.prop1); // Accès à prop1 depuis l'extérieur de la classe
 // // console.log(instance.prop2); // Erreur : Prop2 est privée et ne peut pas être accédée ici
@@ -99,107 +100,107 @@
 // // instance.methodePublique(); // Appel de la méthode publique
 // // instance.methodePrivee(); // Erreur : MethodePrivee est privée et ne peut pas être appelée ici
 // // instance.methodeProtegee(); // Erreur : MethodeProtegee est protégée et ne peut pas être appelée ici
-
 // // const instanceEnfant = new MaClasseEnfant("valeur1", 42, true);
 // // instanceEnfant.utiliserProprietesProtegees(); // Utilisation des propriétés protégées depuis une classe enfant
-
 // import { MaClasse } from './maClasse';
-
 // // Utilisez la classe ici
 // const instance = new MaClasse();
-
-class Hero{
-  private name: string;
-  private power: number;
-  private life: number;
-  weapon!:Weapon;
-
-  constructor(name: string,power: number,life: number){
-    this.name = name;
-    this.power = power;
-    this.life = life;
-  }
-  public attack(opponent: Hero){
-    opponent.reduceLife(this.power);
-  }
-  reduceLife(damage: number){
-    this.life -= damage;
-  }
-  isAlive(): boolean{
-    return this.life>0;
-  }
-  getName(): string{
-    return this.name;
-  }
-  getPower(): number{
-    return this.power;
-  }
-  getLife(): number{
-    return this.life;
-  }
-  setName(name: string){
-    this.name = name;
-  }
-  setPower(power: number){
-    this.power = power;
-  }
-  setLife(life: number){
-    this.life = life;
-  }
-}
-class HeroAxe extends Hero{
-    constructor(name: string, power: number, life: number){
-        super(name,power,life);
-        this.weapon = new Weapon('axe',10);
- 
+var Hero = /** @class */ (function () {
+    function Hero(name, power, life) {
+        this.name = name;
+        this.power = power;
+        this.life = life;
     }
-    public attack(opponent: Hero): void{
-        if (opponent instanceof HeroSword){
-            opponent.reduceLife(this.getPower()*2+this.weapon.damage);
-        }else{
-            opponent.reduceLife(this.getPower()+this.weapon.damage);
+    Hero.prototype.attack = function (opponent) {
+        opponent.reduceLife(this.power);
+    };
+    Hero.prototype.reduceLife = function (damage) {
+        this.life -= damage;
+    };
+    Hero.prototype.isAlive = function () {
+        return this.life > 0;
+    };
+    Hero.prototype.getName = function () {
+        return this.name;
+    };
+    Hero.prototype.getPower = function () {
+        return this.power;
+    };
+    Hero.prototype.getLife = function () {
+        return this.life;
+    };
+    Hero.prototype.setName = function (name) {
+        this.name = name;
+    };
+    Hero.prototype.setPower = function (power) {
+        this.power = power;
+    };
+    Hero.prototype.setLife = function (life) {
+        this.life = life;
+    };
+    return Hero;
+}());
+var HeroAxe = /** @class */ (function (_super) {
+    __extends(HeroAxe, _super);
+    function HeroAxe(name, power, life) {
+        var _this = _super.call(this, name, power, life) || this;
+        _this.weapon = new Weapon('axe', 10);
+        return _this;
+    }
+    HeroAxe.prototype.attack = function (opponent) {
+        if (opponent instanceof HeroSword) {
+            opponent.reduceLife(this.getPower() * 2 + this.weapon.damage);
         }
-    }
-}
-class HeroSword extends Hero{
-    constructor(name: string, power: number, life: number){
-        super(name,power,life);
-        this.weapon = new Weapon('sword',20);
-    }
-    public attack(opponent: Hero): void{
-        if (opponent instanceof HeroSpear){
-            opponent.reduceLife(this.getPower()*2+this.weapon.damage);
-        }else{
-            opponent.reduceLife(this.getPower()+this.weapon.damage);
+        else {
+            opponent.reduceLife(this.getPower() + this.weapon.damage);
         }
+    };
+    return HeroAxe;
+}(Hero));
+var HeroSword = /** @class */ (function (_super) {
+    __extends(HeroSword, _super);
+    function HeroSword(name, power, life) {
+        var _this = _super.call(this, name, power, life) || this;
+        _this.weapon = new Weapon('sword', 20);
+        return _this;
     }
-
-}
-class HeroSpear extends Hero{
-    constructor(name: string, power: number, life: number){
-        super(name,power,life);
-        this.weapon = new Weapon('spear',30);
-    }
-    public attack(opponent: Hero): void{
-        if (opponent instanceof HeroAxe){
-            opponent.reduceLife(this.getPower()*2+this.weapon.damage);
-        }else{
-            opponent.reduceLife(this.getPower()+this.weapon.damage);
+    HeroSword.prototype.attack = function (opponent) {
+        if (opponent instanceof HeroSpear) {
+            opponent.reduceLife(this.getPower() * 2 + this.weapon.damage);
         }
+        else {
+            opponent.reduceLife(this.getPower() + this.weapon.damage);
+        }
+    };
+    return HeroSword;
+}(Hero));
+var HeroSpear = /** @class */ (function (_super) {
+    __extends(HeroSpear, _super);
+    function HeroSpear(name, power, life) {
+        var _this = _super.call(this, name, power, life) || this;
+        _this.weapon = new Weapon('spear', 30);
+        return _this;
     }
-}
-class Weapon{
-    damage: number
-    weapon: String;
-    constructor(weapon: string,damage: number){
+    HeroSpear.prototype.attack = function (opponent) {
+        if (opponent instanceof HeroAxe) {
+            opponent.reduceLife(this.getPower() * 2 + this.weapon.damage);
+        }
+        else {
+            opponent.reduceLife(this.getPower() + this.weapon.damage);
+        }
+    };
+    return HeroSpear;
+}(Hero));
+var Weapon = /** @class */ (function () {
+    function Weapon(weapon, damage) {
         this.weapon = weapon;
         this.damage = damage;
     }
-}
-
-function bataille(hero1: Hero, hero2: Hero): string{
-    while(hero1.isAlive() && hero2.isAlive()){
-        console.log('Attaque '+hero1.getName()+"/"+hero2.getName());
+    return Weapon;
+}());
+function bataille(hero1, hero2) {
+    while (hero1.isAlive() && hero2.isAlive()) {
+        console.log('Attaque ' + hero1.getName() + "/" + hero2.getName());
         console.log(hero1);
         console.log(hero2);
         hero1.attack(hero2);
@@ -208,45 +209,34 @@ function bataille(hero1: Hero, hero2: Hero): string{
         console.log(hero1);
         console.log(hero2);
     }
-    let result: HTMLElement | null = document.getElementById("result")
-    result!.innerHTML = "Résultat";
-    if(!hero1.isAlive() && !hero2.isAlive()){
-        result!.innerHTML = "It's a draw!";
-        return("It's a draw");
-    }else if(!hero1.isAlive()){
-        result!.innerHTML = `${hero2.getName()} wins!`;
-        return(`${hero2.getName()} wins`);
-    }else{
-        result!.innerHTML = `${hero1.getName()} wins!`;
-        return(`${hero1.getName()} wins`);
+    var result = document.getElementById("result");
+    result.innerHTML = "Résultat";
+    if (!hero1.isAlive() && !hero2.isAlive()) {
+        result.innerHTML = "It's a draw!";
+        return ("It's a draw");
+    }
+    else if (!hero1.isAlive()) {
+        result.innerHTML = "".concat(hero2.getName(), " wins!");
+        return ("".concat(hero2.getName(), " wins"));
+    }
+    else {
+        result.innerHTML = "".concat(hero1.getName(), " wins!");
+        return ("".concat(hero1.getName(), " wins"));
     }
 }
-
 // Deux instances de Hero
-const joan = new Hero("Joan",20,100);
-const leon = new Hero("Leon",20,100);
-const jerome = new HeroAxe("Jérôme",20,100 );
-const quentin = new HeroSpear("Quentin",20,100);
-const bayram = new HeroSword("Bayram",20,100);
+var joan = new Hero("Joan", 20, 100);
+var leon = new Hero("Leon", 20, 100);
+var jerome = new HeroAxe("Jérôme", 20, 100);
+var quentin = new HeroSpear("Quentin", 20, 100);
+var bayram = new HeroSword("Bayram", 20, 100);
 // console.log("GAME leon,joan: "+bataille(leon,joan));
 // console.log("GAME quentin,jerome: "+bataille(quentin,jerome));
 // console.log("GAME jerome,jerome: "+bataille(jerome,jerome));
-
-const gameButton: HTMLElement | null = document.getElementById("gameButton");
-const valueHero1: HTMLInputElement | null = document.getElementById("valueHero1");
-const valueHero2: HTMLElement | null = document.getElementById("valueHero2");
-if(gameButton){
-    gameButton.addEventListener("click",() => {
-    const hero1 = `new ${valueHero1!.value}(${valueHero1!.value},20,100)`;
-    console.log("GAME leon,Quentin: "+bataille(leon,quentin));
-  });
-}
-
-
-
-
-
-
+var gameButton = document.getElementById("gameButton");
+gameButton.addEventListener("click", function () {
+    console.log("GAME leon,Quentin: " + bataille(leon, quentin));
+});
 // Début de la partie
 // joan.attack(leon);
 // leon.attack(joan);
@@ -254,9 +244,6 @@ if(gameButton){
 // quentin.attack(jerome);
 // bayram.attack(quentin);
 // bayram.attack(quentin);
-
-
-
 // Résultats
 // JOAN
 // console.log("Nom du joueur", joan.getName());
